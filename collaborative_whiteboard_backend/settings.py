5ce4a49b5592dcb6df0861fe52a394e118097c92
LAB23_USER ON WS07 <L23X16W07@LabHCI.net>
2025-10-29 17:12:27 +0800

    Modifications in middleware and staticfiles in settings.py

--- collaborative_whiteboard_backend/settings.py
+++ collaborative_whiteboard_backend/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'collaborative_whiteboard_backend.urls'
@@ -133,8 +134,12 @@ SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET", "whiteboard_snapshots")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+ 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
